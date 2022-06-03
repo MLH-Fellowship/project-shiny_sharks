d4bdb71413d51e5e9f6917caba1ea783d9427b26
lauren-ciha <64796985+Lauren-Ciha@users.noreply.github.com>
2022-06-03 13:45:08 -0500

    added map page (instructions for creating the map are in map_page.html)

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,25 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html', title="Home Page", url=os.getenv("URL"))
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# I added the extra lines to prevent merge conflicts :)
+map_link = ""
+map_link = "https://www.google.com/maps/d/u/0/embed?mid=1XH7YSZNH1D0C2EyBt-MWoSai8VBj-zg&ehbc=2E312F"
+@app.route('/map_page')
+def map():
+    return render_template('map_page.html', title="Map", map_url = map_link, url=os.getenv("URL"))
